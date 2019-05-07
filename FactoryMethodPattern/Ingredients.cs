105eb0d29b8767fba8450dfa16de80543ab6afde
Akash Acharya <akashnacharya@gmail.com>
2019-05-06 21:59:35 -0400

    Design Patterns GOF

---
+++ FactoryMethodPattern/Ingredients.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    /// <summary>
+    /// Product
+    /// </summary>
+    abstract class Ingredient { }
+
+    /// <summary>
+    /// Concrete Product
+    /// </summary>
+    class Bread : Ingredient { }
+
+    /// <summary>
+    /// Concrete Product
+    /// </summary>
+    class Turkey : Ingredient { }
+
+    /// <summary>
+    /// Concrete Product
+    /// </summary>
+    class Lettuce : Ingredient { }
+
+    /// <summary>
+    /// Concrete Product
+    /// </summary>
+    class Mayonnaise : Ingredient { }
+
+
+    /// <summary>
+    /// Creator
+    /// </summary>
+    abstract class Sandwich
+    {
+        private List<Ingredient> _ingredients = new List<Ingredient>();
+
+        public Sandwich()
+        {
+            CreateIngredients();
+        }
+
+        //Factory method
+        public abstract void CreateIngredients();
+
+        public List<Ingredient> Ingredients
+        {
+            get { return _ingredients; }
+        }
+    }
+
+    /// <summary>
+    /// Concrete Creator
+    /// </summary>
+    class TurkeySandwich : Sandwich
+    {
+        public override void CreateIngredients()
+        {
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Bread());
+        }
+    }
+
+    /// <summary>
+    /// Concrete Creator
+    /// </summary>
+    class Dagwood : Sandwich //OM NOM NOM
+    {
+        public override void CreateIngredients()
+        {
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Turkey());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Lettuce());
+            Ingredients.Add(new Mayonnaise());
+            Ingredients.Add(new Bread());
+        }
+    }
+}
---
+++ Singleton/TheBell.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public sealed class TheBell
+    {
+        private static TheBell bellConnection;
+        private static object syncRoot = new Object();
+        private TheBell()
+        {
+
+        }
+
+        /// <summary>
+        /// We implement this method to ensure thread safety for our singleton.
+        /// </summary>
+        public static TheBell Instance
+        {
+            get
+            {
+                lock(syncRoot)
+                {
+                    if(bellConnection == null)
+                    {
+                        bellConnection = new TheBell();
+                    }
+                }
+
+                return bellConnection;
+            }
+        }
+
+        public void Ring()
+        {
+            Console.WriteLine("Ding! Order up!");
+        }
+    }
+}
